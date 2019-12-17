a886b270e1708ca1ab46406031197a801cb97f36
Assfugil <42699250+Assfugil@users.noreply.github.com>
2019-12-17 23:48:19 +0800

    Delete http.js

--- config/http.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Ping! Nick Chan#5213\'s main script is working'); //write a response to the client
-  res.end(); //end the response
-}).listen(81)
