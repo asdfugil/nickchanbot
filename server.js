218548f4300843578cf5915f3b866e3b7899a0ec
Glitch (nickchandev-server) <none>
2020-02-16 14:06:10 +0000

    🦃🏛 Checkpoint ./server.js:6429734/626

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const Keyv = require('keyv')
+const ranks = new Keyv("sqlite://.data/database.sqlite", {
+  namespace: "ranks"
+});
 app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.send('OK');
@@ -9,4 +13,11 @@ app.listen(process.env.PORT, function () {
   console.log('Example app listening on port' + process.env.PORT+"!");
 });
 app.use(express.static('public'));
+app.get('/api/v0/ranks',(req,res) => {
+    const guild_id = request.query.guild_id
+    if (!guild_id) {
+      response.send('{"message":"guild_id is a required argument that is missing"}')
+      response.end()
+    }
+})
 require('./index.js')
