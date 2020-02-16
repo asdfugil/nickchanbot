5d43ea2ebc314431408a7d90f54575d932e148fa
Glitch (nickchandev-server) <none>
2020-02-16 14:18:02 +0000

    🐣🎰 Checkpoint ./server.js:6429734/100 ./watch.json:6429734/2

--- server.js
+++ server.js
@@ -13,11 +13,14 @@ app.listen(process.env.PORT, function () {
   console.log('Example app listening on port' + process.env.PORT+"!");
 });
 app.use(express.static('public'));
-app.get('/api/v0/ranks',(req,res) => {
-    const guild_id = request.query.guild_id
+app.get('/api/v0/ranks',async (req,res) => {
+    const guild_id = req.query.guild_id
     if (!guild_id) {
-      response.send('{"message":"guild_id is a required argument that is missing"}')
-      response.end()
+      res.send('{"message":"guild_id is a required argument that is missing"}')
+      res.end()
     }
+  const data = await ranks.get(guild_id)
+  res.send(data)
+  res.end()
 })
 require('./index.js')
