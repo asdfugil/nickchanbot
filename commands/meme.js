36ab1b380949d7d24d111c8a41fe021f355c6395
Glitch (nickchandev-server) <none>
2020-02-20 10:09:12 +0000

    🎿🔔 Checkpoint ./commands/meme.js:5579006/3393

--- commands/meme.js
+++ commands/meme.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const fetch = require('node-fetch');
-const { RichEmbed, Attachment } = require('discord.js');
-const subreddits = ["dankmemes", "memes", "MemeEconomy", "PrequelMemes", "PewdiepieSubmissions", "funny", "wholesomememes", "2meirl4meirl", "me_irl"];
+const { RichEmbed } = require('discord.js');
+const subreddits = ["dankmemes", "memes", "PrequelMemes", "PewdiepieSubmissions", "funny", "wholesomememes", "2meirl4meirl", "me_irl"];
 
 module.exports = {
 	name: 'meme',
@@ -10,7 +10,6 @@ module.exports = {
   cooldown: 1,
 	async execute(message) {
     const asubreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
-    (async () => {
       const ameme = await fetch(`https://imgur.com/r/${asubreddit}/hot.json`).then(response => response.json());
       if (!ameme.data) return;
       const imgurData = ameme.data[Math.floor(Math.random() * ameme.data.length)];
@@ -20,12 +19,11 @@ module.exports = {
       if (!image || image.includes(".mp4")) return message.channel.send(`Woah too fast, Try again.`);
       const memeEmbed = new RichEmbed()
       .setColor('RANDOM')
-      //.setTitle(imgurData.title)
-      //.setURL(`https://www.reddit.com${imgurData.reddit}`)
+      .setTitle(imgurData.title)
+      .setURL(`https://www.reddit.com${imgurData.reddit}`)
       .setImage(image)
-      .setFooter(message.author.tag, `from /r/${asubreddit} - Requested by ${message.author.tag}`)
+      .setFooter(`from /r/${asubreddit} - Requested by ${message.author.tag}`, message.author.displayAvatarURL)
       .setTimestamp()
       message.channel.send(memeEmbed);
-    })();
   },
 };
