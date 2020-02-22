d4c27015c25833e4d54623bd5795e9e82d13c61b
Glitch (nickchandev-server) <none>
2020-02-22 08:18:44 +0000

    🚖👬 Checkpoint ./commands/invite-info.js:6429734/115 ./watch.json:6429734/6

--- commands/invite-info.js
+++ commands/invite-info.js
@@ -86,13 +86,14 @@ User ID:${invite.inviter.id}
           message.channel.stopTyping();
         });
     } else {
+      console.log(directFetch)
       const embed = new RichEmbed()
         .setTitle(directFetch.channel.name || directFetch.recipients.map(x => x.username).join(','))
         .setURL(`https://discord.gg/${directFetch.code}`)
         .addField("Member Count", directFetch.approximate_member_count)
         .addField("Group DM Info", `**Name**:${directFetch.channel.name || "N/A"}
 **ID:** ${directFetch.channel.id}
-**recipients:** ${directFetch.recipients.map(x => x.username).join(",")}`)
+**recipients:** ${directFetch.channel.recipients.map(x => x.username).join(",")}`)
         .addField("Inviter Tag", `${directFetch.inviter.username}#${directFetch.inviter.discriminator}`)
         .addField("Inviter ID", directFetch.inviter.id)
         .setColor("RANDOM")
