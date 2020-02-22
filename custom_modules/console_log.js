b44b571dc2e8941fb3dbdf49a6bea24cca6ea326
Glitch (nickchandev-server) <none>
2020-02-22 04:47:01 +0000

    ⛈🚝 Checkpoint ./custom_modules/console_log.js:6429734/654 ./bot.js:6429734/100 ./watch.json:6429734/4

--- custom_modules/console_log.js
+++
@@ -1,5 +0,0 @@
-const { WebhookClient } = require('discord.js')
-const stdout = new WebhookClient('680631969830141968','0o2pnuKcuEVATglfcCZ30mW7fqVpHkO36AyuSRoWS_ARRWxg3zmV0VMOROvzBO_jnLVI')
-module.exports = () => {
-  process.stdout.on("data",data => stdout.send(data.toString()))
-}
