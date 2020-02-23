3283f97f3d089cf14557df2dcf2692352b4e121c
Glitch (nickchandev-server) <none>
2020-02-23 12:14:12 +0000

    🏏🌊 Checkpoint ./commands/8ball.js:6429734/591

---
+++ commands/8ball.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+const fetch = require('node-fetch');
+
+module.exports = {
+	name: '8ball',
+	description: 'Ask your questions to the magical 8ball .',
+  aliases: ['8-ball', 'eightball', 'eight-ball'],
+  cooldown: 0.1,
+	execute(message, args) {
+    (async () => {
+      const { response } = await fetch('https://nekos.life/api/v2/8ball').then(response => response.json());
+      if (!args.length) {
+        return message.channel.send('Please specify the arguments for 8ball to answer!');
+      }
+      message.channel.send(response);
+    })();
+  },
+};
+// Special thanks to FAV
