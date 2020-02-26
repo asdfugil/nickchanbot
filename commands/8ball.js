245693a8c3f15c0e8b47bd3f5e7fb9ac814132b6
Glitch (nickchandev-server) <none>
2020-02-26 12:19:31 +0000

    🗻⏱ Checkpoint ./commands/bio.js:6429734/9 ./commands/changelogs.js:6429734/848 ./commands/8ball.js:6429734/1228 ./watch.json:6429734/4

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,19 +1,11 @@
-const moment = require('moment');
-const fetch = require('node-fetch');
-
+const fetch = require("node-fetch");
 module.exports = {
-	name: '8ball',
-	description: 'Ask your questions to the magical 8ball .',
-  aliases: ['8-ball', 'eightball', 'eight-ball'],
-  cooldown: 3,
-	execute(message, args) {
-    (async () => {
-      const { response } = await fetch('https://nekos.life/api/v2/8ball').then(response => response.json());
-      if (!args.length) {
-        return message.channel.send('Please specify the arguments for 8ball to answer!');
-      }
-      message.channel.send(response);
-    })();
-  },
+  name: "8ball",
+  description: "Ask your questions to the magical 8ball .",
+  aliases: ["8-ball", "eightball", "eight-ball"],
+  cooldown: 1,
+  args: 1,
+  async execute(message, args) { message.channel.send((await fetch("https://nekos.life/api/v2/8ball").then(response => response.json())).response);
+  }
 };
 // Special thanks to FAV
