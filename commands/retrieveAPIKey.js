77de0dc6f343c45f866347e633cdf9d852e24c9b
Glitch (nickchandev-server) <none>
2020-02-28 05:26:17 +0000

    👓⛩ Checkpoint ./commands/generateAPIKey.js:6429734/420 ./commands/retrieveAPIKey.js:6429734/1634

--- commands/retrieveAPIKey.js
+++ commands/retrieveAPIKey.js
@@ -7,11 +7,8 @@ module.exports = {
     async execute(message,args) {
         const keys = await Nkeys.get("keys") || Object.create(null)
         const existingKey = Object.keys(keys).find(x => keys[x] === message.author.id)
-        if (existingKey) delete keys[existingKey]
-        const key = message.client.commands.get("randomstring").random(64)
-        keys[key] = message.author.id
-        Nkeys.set("keys",keys)
-        message.author.send(key).catch(error => { 
+        if (existingKey) return message.reply("You don't have an API key yet.")
+        message.author.send(existingKey).catch(error => { 
             if (error.code === 50007) message.reply("Please enable your DM.")
             else throw error
         })
