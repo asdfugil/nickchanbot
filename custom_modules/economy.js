b70ece4b5c94ba1d753b103cea1f5bc5cfcc8701
Glitch (nickchandev-server) <none>
2020-02-28 02:15:21 +0000

    🎆🗺 Checkpoint ./custom_modules/economy.js:6429734/568

--- custom_modules/economy.js
+++ custom_modules/economy.js
@@ -25,6 +25,11 @@ module.exports = {
          * @param { number? } amount amount of the item to be given to the user
          */
         giveItem(name,amount) { this.inv[name] ? this.inv[name] += (amount || 1) : this.inv[name] = (amount || 1) }
-
+        /**
+         * Take (an) item(s) from the user.
+         * @param { string } name - Name of the item
+         * @param { number? | "all"? } amount amount of the item to be given to the user
+         */
+        takeItem(name,amount) { ((amount || "").toLowerCase()) === 'all' ? delete this.inv[name] : this.inv.name -= (amount || 1)}
     }
 }
