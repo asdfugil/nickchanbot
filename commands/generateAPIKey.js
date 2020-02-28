1c6d589a786f7ff55515d27bf2f5a30893c0f568
Glitch (nickchandev-server) <none>
2020-02-28 05:14:18 +0000

    📉🍲 Checkpoint ./commands/generateAPIKey.js:6429734/774

--- commands/generateAPIKey.js
+++ commands/generateAPIKey.js
@@ -1,9 +1,15 @@
-const keys = new (require("keyv"))("sqlite://.data/database.sqlite",{namespace:"api-keys"})
+const Nkeys = new (require("keyv"))("sqlite://.data/database.sqlite",{namespace:"api-keys"})
 module.exports = {
     name:'generateAPIKey',
     description:'generate an Nick Chan Bot API key',
     cooldown:30,
     async execute(message,args) {
-        consrt keymessage.client.commands.get("randomstring").random(64)
+        const keys = await Nkeys.get("keys") || Object.create(null)
+        const key = message.client.commands.get("randomstring").random(64)
+        keys[key] = message.author.id
+        Nkeys.set("keys",keys)
+        message.author.send(key).catch(error => { 
+            if (error.code === 50007) 
+        })
     }
 }
