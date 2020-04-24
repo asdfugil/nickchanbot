8d2d2a0af7a795e14356cbf22bac14dc49ba4fa5
Glitch (nickchanbot-server) <none>
2020-04-24 08:14:31 +0000

    🍩🖼 Checkpoint ./index.js:8621426/136 ./watch.json:8621426/4

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ require('dotenv').config()
 const { ShardingManager } = require('discord.js')
 const { TOKEN } = process.env
 const manager = new ShardingManager('./bot.js', {token:TOKEN})
-console.log("| |/ /__ _ __| |_ (_)_ __  __ _\n| ' </ _` (_-< ' \\| | '  \\/ _` |\n|_|\\_\\__,_/__/_||_|_|_|_|_\\__,_|\n");
-
-manager.spawn(1,4000, false)
+manager.spawn(1, 4000)
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}.`));
 manager.on("message",(shard,receivedMessage) => {
   console.log("Message received from shard " + shard.id)
