8b72cd7134dbb6704a78dd140e25ae11ad205cb5
Glitch (nickchanbot-server) <none>
2020-04-25 02:20:25 +0000

    🚙🎗 Checkpoint ./server.js:6429734/2 ./package.json:6429734/10 ./watch.json:6429734/4 ./bot.js:6429734/33

--- bot.js
+++ bot.js
@@ -21,7 +21,6 @@ class NickChanBotClient extends Discord.Client {
   }
 }
 const client = new NickChanBotClient({ http:{ version:7 } });
-client.on('debug', console.log);
 const Keyv = require("keyv");
 const prefixs = new Keyv("sqlite://.data/database.sqlite", {
   namespace: "prefixs"
