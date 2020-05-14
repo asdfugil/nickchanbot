9add73827d2e97d78074fe12e70168916e51b6c9
Glitch (nickchandev-server) <none>
2020-05-14 10:32:25 +0000

    🎭🚉 Checkpoint ./server.js:6429734/2672 ./package.json:6429734/3779 ./watch.json:6429734/377 ./index.js:6429734/1087

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ console.log("Launching shard manager...");
 require("dotenv").config();
 const { ShardingManager, WebhookClient } = require("discord.js");
 const { TOKEN } = process.env;
-const manager = new ShardingManager("./bot.js", { token: TOKEN,mode:'process' });
-manager.spawn(1, 4000)
+const manager = new ShardingManager("./bot.js", {
+  token: TOKEN,
+  mode: "process"
+});
+manager.spawn(1, 4000);
 manager.on("launch", shard => console.log(`Launched shard ${shard.id}.`));
 manager.on("message", (shard, receivedMessage) => {
   console.log("Message received from shard " + shard.id);
