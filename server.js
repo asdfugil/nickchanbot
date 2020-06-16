2d3ac1368e0135e99f7649b94aca57c52184b69d
Glitch (nickchanbot-server) <none>
2020-06-16 06:01:13 +0000

    👨‍👨‍👦⛪️ Checkpoint ./server.js:6429734/2311 ./watch.json:6429734/4

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const Keyv = require('keyv')
-const proxy = require('express-http-proxy')
+const Keyv = require("keyv");
+const proxy = require("express-http-proxy");
 const ranks = new Keyv("sqlite://.data/database.sqlite", {
   namespace: "ranks"
 });
-const fetch = require('node-fetch')
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-  res.send('OK');
+const fetch = require("node-fetch");
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+  res.send("OK");
 });
-app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port' + process.env.PORT+"!");
+app.listen(process.env.PORT, function() {
+  console.log("Example app listening on port" + process.env.PORT + "!");
 });
-app.use(express.static('public'));
-app.get('/api/v0/ranks',async (req,res) => {
-  res.set("Content-Type","application/json; charset=utf-8")
-    const guild_id = req.query.guild_id
-    if (!guild_id) {
-      res.status(400).send('{"message":"guild_id is a required argument that is missing"}')
-      res.end()
-    }
-  const data = await ranks.get(guild_id)
-  res.send(data)
-  res.end()
-})
+app.use(express.static("public"));
+app.get("/api/v0/ranks", async (req, res) => {
+  res.set("Content-Type", "application/json; charset=utf-8");
+  const guild_id = req.query.guild_id;
+  if (!guild_id) {
+    res
+      .status(400)
+      .send('{"message":"guild_id is a required argument that is missing"}');
+    res.end();
+  }
+  const data = await ranks.get(guild_id);
+  res.send(data);
+  res.end();
+});
 
-app.get('/api/secret/37712745',proxy('localhost:1084'))
+app.get("/api/secret/37712745", proxy("localhost:1084"));
 setInterval(() => {
-fetch(`https://${process.env.PROJECT_DOMAIN}.glitch.me`)
-},60000)
-require('./index.js')
+  fetch(`https://${process.env.PROJECT_DOMAIN}.glitch.me`);
+  fetch(`https://boot-my-projects.glitch.me`);
+}, 60000);
+require("./index.js");
