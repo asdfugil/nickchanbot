1abf889023aa7db25b267fa0d52af618ffb41979
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-06-13 05:21:15 +0000

    improve volume command

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -8,11 +8,12 @@ module.exports = {
 	async execute(message,args) {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to adjust/view the volume!');
-		if (!serverQueue) return message.channel.send('There is nothing playing!');
-    if (!args[0]) return message.channel.send("Volume = " + serverQueue.volume)
+	if (!serverQueue) return message.channel.send('There is nothing playing!');
+    if (!args[0]) return message.channel.send("The current volume is " + (serverQueue.volume * 100) + "%")
     if (isNaN(parseFloat(args[0]))) return message.reply("That's not a valid volume.")
     if (parseFloat(args[0]) < 0) return message.reply("Negative volume not yet supported.")
-		serverQueue.volume = parseFloat(args[0])/100
+	if (parseFloat(args[0]) > 1000) return message.reply("maximum volume is 1000%")
+	serverQueue.volume = parseFloat(args[0])/100
     serverQueue.connection.dispatcher.setVolume(parseFloat(args[0])/100)
     message.reply("Volume set to "+args[0] + `%`)
     if (parseFloat(args[0]) > 250) message.reply("Warning:Setting the volume to a value larger than 250% may degrade audio quality.")
