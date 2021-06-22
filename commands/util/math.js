83492d11638b1c437d499e20779a113f8cd9517e
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-06-22 10:20:06 +0000

    fix math

--- commands/util/math.js
+++ commands/util/math.js
@@ -7,7 +7,7 @@ module.exports = {
   description:{ en: 'a calculator' },
   cooldown:1.5,
   aliases:['calc','calculator'],
-  usage:' <expression>',
+  usage:{ en: ' <expression>' },
   info:{ en:`How to write expressions: https://mathjs.org/docs/expressions/syntax.html
 evalulate : a simple calculator
 simplify : simplify algebraic expressions
@@ -15,8 +15,7 @@ simplify : simplify algebraic expressions
   execute:async (message,args) => { 
     try {
     const result = math.evaluate(args.join(" "))
-    console.log(result)
-       message.channel.send(result.toString() | 'undefined',{code:'xl'})
+       message.channel.send(result?.toString() || 'undefined',{code:'xl'})
     } catch (error) {
     message.channel.send(`${error.name}:${error.message}`,{code:'xl'})
   }
